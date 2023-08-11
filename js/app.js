a38c5b89d90b1e2dd9ec3a5d95fa6914f35bc351
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-08-11 10:53:22 -0500

    Remove app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import { default as DEER } from './deer-config.js'
-import { default as UTILS } from './deer-utils.js'
-import './layout.js'
-
-// Generalized feedback, maybe shouldn't be here.
-document.addEventListener('deer-updated', event => {
-    globalFeedbackBlip(event, `Saving ${event.detail.name ? "'" + event.detail.name + "' " : ""}successful!`, true)
-})
