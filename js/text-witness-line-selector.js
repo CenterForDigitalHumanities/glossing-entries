7fc809a512714f37b9cc9970200b742af13f4146
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-09-15 11:37:25 -0500

    A good stubbing now.  Loads all the UI components and will load a plain text resource

--- js/text-witness-line-selector.js
+++ js/text-witness-line-selector.js
@@ -84,148 +84,127 @@ class WitnessTextSelector extends HTMLElement {
             }
         })
         fetch(witnessURI)
-            .then(response => response.json())
-            .then(ms => {
-                let allLines = []
-                const witnessText = $this.querySelector(".witnessText")
-                ms.sequences[0].canvases.forEach((canvas, index) => {
-                    const pageContainer = document.createElement("div")
-                    pageContainer.classList.add("pageContainer")
-                    const pageHeader = document.createElement("h4")
-                    const pageToggle = document.createElement("div")
-                    pageToggle.classList.add("togglePage")
-                    pageToggle.setAttribute("title", "Collapse this page")
-                    pageToggle.addEventListener("click", event => {
-                        const container = event.target.nextElementSibling
-                        if(container.classList.contains("is-toggled")) {
-                            event.target.innerHTML = "&#9660;"
-                            event.target.classList.remove("is-toggled")
-                            container.classList.remove("is-toggled")
-                        }
-                        else{
-                            event.target.innerHTML = "&#9664;"
-                            event.target.classList.add("is-toggled")
-                            container.classList.add("is-toggled")
-                        }
-                    })
-                    pageToggle.innerHTML = `&#9660;`
-                    pageHeader.setAttribute("tpen-canvas-id", canvas["@id"])
-                    pageHeader.innerText = `${canvas.label ?? "No Page Label"} (Page ${index+1})`
-                    witnessText.appendChild(pageHeader)
-                    witnessText.appendChild(pageToggle)
-                    if(canvas?.otherContent[0]?.resources) allLines.concat(canvas.otherContent[0].resources)
-                    canvas.otherContent[0].resources.forEach(line => {
-                        const lineElem = document.createElement("div")
-                        lineElem.setAttribute("title", line["@id"])
-                        lineElem.setAttribute("witness-text-id", line["@id"])
-                        lineElem.setAttribute("tpen-image-url", canvas.images[0].resource["@id"])
-                        lineElem.setAttribute("tpen-image-fragment", line.on.split("#").pop())
-                        lineElem.setAttribute("witness-text-note", line._tpen_note)
-                        lineElem.setAttribute("witness-text-creator", line._tpen_creator)
-                        const txt = line.resource["cnt:chars"] ? line.resource["cnt:chars"] : ""
-                        lineElem.innerText = txt
-                        if(!txt) lineElem.classList.add("emptyLine")
-                        lineElem.onmouseup = function(e) {
-                            const s = document.getSelection()
-                            const customKey = $this.querySelector("input[custom-key='selections']")
-                            const filter = document.querySelector("input[filter]")
-                            const selectedText = document.getSelection() ? document.getSelection().toString().trim() : ""
-                            const firstword = selectedText.split(" ")[0]
-                            if(selectedText){
-                                // The filter may not be in the DOM when the user is selecting text.
-                                // Only use the filter if it is !.is-hidden
-                                if(filter && !filter.classList.contains("is-hidden")){
-                                    filter.value = firstword
-                                    filter.setAttribute("value", firstword)
-                                    filter.dispatchEvent(new Event('input', { bubbles: true }))
-                                }
+            .then(response => response.text())
+            .then(witness_text_data => {
+                const witnessTextElem = $this.querySelector(".witnessText")
+                let structured = false
+                // TODO check if it is formatted text.  Is it JSON?  XML?  TEI-XML?
+                if(structured){
+                    // If it is something we can detect has "pages", let's make a good page-by-page UI
+                    witness_text_data.pages.forEach((page, index) => {
+                        const pageContainer = document.createElement("div")
+                        pageContainer.classList.add("pageContainer")
+                        const pageHeader = document.createElement("h4")
+                        const pageToggle = document.createElement("div")
+                        pageToggle.classList.add("togglePage")
+                        pageToggle.setAttribute("title", "Collapse this page")
+                        pageToggle.addEventListener("click", event => {
+                            const container = event.target.nextElementSibling
+                            if(container.classList.contains("is-toggled")) {
+                                event.target.innerHTML = "&#9660;"
+                                event.target.classList.remove("is-toggled")
+                                container.classList.remove("is-toggled")
+                            }
+                            else{
+                                event.target.innerHTML = "&#9664;"
+                                event.target.classList.add("is-toggled")
+                                container.classList.add("is-toggled")
+                            }
+                        })
+                        pageToggle.innerHTML = `&#9660;`
+                        pageHeader.setAttribute("tpen-canvas-id", canvas["@id"])
+                        pageHeader.innerText = `${canvas.label ?? "No Page Label"} (Page ${index+1})`
+                        witnessTextElem.appendChild(pageHeader)
+                        witnessTextElem.appendChild(pageToggle)
 
-                                const textInput = document.querySelector("textarea[custom-text-key='text']")
-                                textInput.setAttribute("value", selectedText)
-                                textInput.value = selectedText
-                                textInput.dispatchEvent(new Event('input', { bubbles: true }))
+                        // Is this page further divided into lines?
+                        let allLines = []
+                        if(page.lines) allLines.concat(canvas.otherContent[0].resources)
+                        lines.forEach(line => {
+                            const lineElem = document.createElement("div")
+                            const txt = line.text ?? ""
+                            lineElem.setAttribute("text", line.text)
+                            lineElem.innerText = line.text
+                            if(!txt) lineElem.classList.add("emptyLine")
+                            lineElem.onmouseup = function(e) {
+                                const s = document.getSelection()
+                                const customKey = $this.querySelector("input[custom-key='selections']")
+                                const filter = document.querySelector("input[filter]")
+                                const selectedText = document.getSelection() ? document.getSelection().toString().trim() : ""
+                                const firstword = selectedText.split(" ")[0]
+                                if(selectedText){
+                                    // The filter may not be in the DOM when the user is selecting text.
+                                    // Only use the filter if it is !.is-hidden
+                                    if(filter && !filter.classList.contains("is-hidden")){
+                                        filter.value = firstword
+                                        filter.setAttribute("value", firstword)
+                                        filter.dispatchEvent(new Event('input', { bubbles: true }))
+                                    }
 
-                                let witnessLabel = selectedText.slice(0, 16)
-                                const labelElem = document.querySelector("input[deer-key='label']")
-                                const shelfmark = document.querySelector("input[deer-key='identifier']").value
-                                // Generate a programmatic label
-                                if(witnessLabel){
-                                    if(shelfmark){
-                                        witnessLabel += `...(${shelfmark})`
-                                    }
-                                    else{
-                                        witnessLabel += `...(${Date.now()})`
-                                    }    
-                                    if(labelElem.value !== witnessLabel){
-                                        labelElem.value = witnessLabel
-                                        labelElem.setAttribute("value", witnessLabel)
-                                        labelElem.dispatchEvent(new Event('input', { bubbles: true }))
-                                    }
-                                }
-                                else{
-                                   // A side effect of this is that a label cannot be unset by a typical DEER form update.
-                                   labelElem.value = "" 
-                                   labelElem.setAttribute("value", "")
-                                   labelElem.$isDirty = false
-                                }
-                                let selections = []
-                                let linePreviews = []
-                                const stopID = document.getSelection().extentNode.parentElement.getAttribute("witness-text-id")
-                                let el = document.getSelection().baseNode.parentElement
-                                let stopEl = document.getSelection().extentNode.parentElement
-                                $this.querySelectorAll(".togglePage").forEach(tog => tog.classList.remove("has-selection"))
-                                el.parentElement.previousElementSibling.classList.add("has-selection")
-                                stopEl.parentElement.previousElementSibling.classList.add("has-selection")
-                                if(stopID === el.getAttribute("witness-text-id")){
-                                    // The entire selection happened in just this line.  It will not be empty.
-                                    selections.push(`${el.getAttribute("witness-text-id")}#char=${document.getSelection().baseOffset},${document.getSelection().extentOffset}`)
-                                    linePreviews.push()
-                                }
-                                else{
-                                    // The selection happened over multiple lines.  We need to make a target out of each line.  There may be empty lines in-between.
-                                    if(!el.classList.contains("emptyLine")) selections.push(`${el.getAttribute("witness-text-id")}#char=${document.getSelection().baseOffset},${el.innerText.length-1}`)
-                                    el = el.nextElementSibling
-                                    while(el.getAttribute("witness-text-id") !== stopID){
-                                        if(!el.classList.contains("emptyLine")){
-                                            selections.push(`${el.getAttribute("witness-text-id")}#char=0,${el.innerText.length-1}`)
-                                        }
-                                        if(el.nextElementSibling){
-                                            el = el.nextElementSibling    
+                                    const textInput = document.querySelector("textarea[custom-text-key='text']")
+                                    textInput.setAttribute("value", selectedText)
+                                    textInput.value = selectedText
+                                    textInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+                                    let witnessLabel = selectedText.slice(0, 16)
+                                    const labelElem = document.querySelector("input[deer-key='label']")
+                                    const shelfmark = document.querySelector("input[deer-key='identifier']").value
+                                    // Generate a programmatic label
+                                    if(witnessLabel){
+                                        if(shelfmark){
+                                            witnessLabel += `...(${shelfmark})`
                                         }
                                         else{
-                                            //We are at the end of a page and are going on to the next page.  Get to the next page and get the firtst line
-                                            el = el.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.firstChild
+                                            witnessLabel += `...(${Date.now()})`
+                                        }    
+                                        if(labelElem.value !== witnessLabel){
+                                            labelElem.value = witnessLabel
+                                            labelElem.setAttribute("value", witnessLabel)
+                                            labelElem.dispatchEvent(new Event('input', { bubbles: true }))
                                         }
-                                    }  
-                                    if(!el.classList.contains("emptyLine")){
-                                        selections.push(`${el.getAttribute("witness-text-id")}#char=0,${document.getSelection().extentOffset}`)
                                     }
-                                }
-                                if(customKey.value !== selections.join("__")){
-                                    customKey.value = selections.join("__") 
-                                    customKey.$isDirty = true
-                                    $this.closest("form").$isDirty = true
-                                }
-                                $this.querySelectorAll(".togglePage:not(.has-selection)").forEach(tog => {
-                                    if(!tog.classList.contains("is-toggled")){
-                                        tog.click()
+                                    else{
+                                       // A side effect of this is that a label cannot be unset by a typical DEER form update.
+                                       labelElem.value = "" 
+                                       labelElem.setAttribute("value", "")
+                                       labelElem.$isDirty = false
                                     }
-                                })    
-                                console.log("You made the following line selections")
-                                console.log(selections)
+                                    let selections = []
+                                    let linePreviews = []
+                                    const stopID = document.getSelection().extentNode.parentElement.getAttribute("witness-text-id")
+                                    let el = document.getSelection().baseNode.parentElement
+                                    let stopEl = document.getSelection().extentNode.parentElement
+                                    $this.querySelectorAll(".togglePage").forEach(tog => tog.classList.remove("has-selection"))
+                                    el.parentElement.previousElementSibling.classList.add("has-selection")
+                                    stopEl.parentElement.previousElementSibling.classList.add("has-selection")
+                                    $this.querySelectorAll(".togglePage:not(.has-selection)").forEach(tog => {
+                                        if(!tog.classList.contains("is-toggled")){
+                                            tog.click()
+                                        }
+                                    })    
+                                    console.log("You made the following line selections")
+                                    console.log(selections)
+                                }
                             }
-                        }
-                        pageContainer.appendChild(lineElem)
-                    })
-                    witnessText.appendChild(pageContainer)
-                })
-                const e = new CustomEvent("witness-texts-loaded", {bubbles: true })
+                            pageContainer.appendChild(lineElem)
+                        })
+                        witnessTextElem.appendChild(pageContainer)
+                    })                   
+                }
+                else{
+                    // If we couldn't tell what kind of resource it was, treat it as plain text
+                    const plaintext = document.createElement("div")
+                    plaintext.setAttribute("witness-uri", witnessURI)
+                    plaintext.innerText = witness_text_data
+                    witnessTextElem.appendChild(plaintext)
+                }
+                const e = new CustomEvent("witness-text-loaded", {bubbles: true })
                 document.dispatchEvent(e)
-                $this.setAttribute("witness-texts-loaded", "true")
+                $this.setAttribute("witness-text-loaded", "true")
             })
             .catch(err => {
                 console.error(err)
-                witnessText.innerHTML = `<b class="text-error"> Could not get T-PEN project ${witnessURI} </b>`
+                witnessTextElem.innerHTML = `<b class="text-error"> Could not get Witness Text Data from ${witnessURI} </b>`
             })
     }
     static get observedAttributes() { return ['witness-uri'] }
