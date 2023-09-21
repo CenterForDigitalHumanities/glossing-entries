151ee21fa5343e607b5a4e194d7e41b563fa76cc
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-09-21 13:53:01 -0500

    Get rid of testing user overrides

--- js/shared.js
+++ js/shared.js
@@ -3,16 +3,16 @@
  */
 
 //For when we test, so we can easily find and blow away junk data
-setTimeout(() => {
-    document.querySelectorAll("input[deer-key='creator']").forEach(el => {
-        el.value="ReleaseInterfaceTest"
-        el.setAttribute("value", "ReleaseInterfaceTest")
-    })
-    document.querySelectorAll("form").forEach(el => {
-        el.setAttribute("deer-creator", "ReleaseInterfaceTest")
-    })
-    window.GOG_USER["http://store.rerum.io/agent"] = "ReleaseInterfaceTest"
-}, 4000)
+// setTimeout(() => {
+//     document.querySelectorAll("input[deer-key='creator']").forEach(el => {
+//         el.value="ReleaseInterfaceTest"
+//         el.setAttribute("value", "ReleaseInterfaceTest")
+//     })
+//     document.querySelectorAll("form").forEach(el => {
+//         el.setAttribute("deer-creator", "ReleaseInterfaceTest")
+//     })
+//     window.GOG_USER["http://store.rerum.io/agent"] = "ReleaseInterfaceTest"
+// }, 4000)
 
 let __constants = {}
 setConstants()
