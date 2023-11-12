2d36c45be501cc0db38d261c7d95cde60d79a0ba
BuildTools <unconfigured@null.spigotmc.org>
2023-11-11 21:01:13 -0600

    Added scroll feature

--- js/glossModal.js
+++ js/glossModal.js
@@ -101,27 +101,43 @@ customElements.define('gloss-modal-button', GlossModalActivationButton)
 class GlossModal extends HTMLElement {
     template = `
         <style>
-            gloss-modal{
-                position: absolute;
-                display: block;
-                top: 1em;
-                z-index: 2;
+        gloss-modal {
+            position: fixed; 
+            display: flex; 
+            align-items: center; 
+            justify-content: center; 
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 1000;
+            overflow: auto; 
+        }
+        .window-shadow {
+            position: fixed;
+            background-color: rgba(0,0,0,0.5);
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+        .modal {
+            position: relative;
+            margin: auto;
+            max-height: 90vh; 
+            overflow-y: auto; 
+            width: auto; 
+            box-sizing: border-box; 
+        }
+        .modal form {
+            padding-bottom: 2em; 
+        }
+        @media (max-width: 768px) {
+            .modal {
+                width: 90%;
+                max-height: 85vh;
             }
-            .window-shadow{
-                position: fixed;
-                background-color: rgba(0,0,0,0.5);
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-            }
-            .modal{
-                position: relative;
-                top: 3em;
-            }
-            .modal form{
-
-            }
+        }
         </style>
         <div class="window-shadow"> 
             <div class="modal container">
