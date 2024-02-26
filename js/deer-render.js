d1a5884d9f77b3fa0a4f7db550974b7e53dbdab8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-26 15:02:16 -0600

    Finalizing

--- js/deer-render.js
+++ js/deer-render.js
@@ -1251,6 +1251,11 @@ DEER.TEMPLATES.managedlist = function (obj, options = {}) {
                 }
                 queryString = queryString.trim()
                 const query = decodeContentState(queryString)
+                for (const prop in query) {
+                    if (typeof query[prop] === 'string') {
+                        query[prop] = query[prop].trim()
+                    }
+                }
                 const items = elem.querySelectorAll('li')
                 items.forEach(li => {
                     const templateContainer = li.parentElement.hasAttribute("deer-template") ? li.parentElement : null
