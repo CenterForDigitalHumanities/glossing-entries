d68812d9268a31f7608f01bd933a3ec8cbde265a
bleesman21 <bradley.leesman@slu.edu>
2024-02-26 19:13:39 -0600

    Cleanup

--- js/deer-config.js
+++ js/deer-config.js
@@ -165,7 +165,7 @@ export default {
 					<input id="search-bar" filter="title" type="text" deer-key="title" placeholder="&hellip;Type to filter by incipit, text, or targeted text" class="is-hidden serifText col">
 					<input type="hidden" deer-key="targetCollection" value="GoG-Named-Glosses">
 					<input is="auth-creator" type="hidden" deer-key="creator" />
-					<input id="search-submit" type="submit" value="Add NG : Title Only" class="is-hidden serifText col" disabled>
+					<input id="search-submit" type="submit" value="New Gloss" class="is-hidden serifText col" disabled>
 				</form>
                 <div class="progressArea">
                     <p class="filterNotice is-hidden"> Gloss filter detected.  Please note that Glosses will appear as they are fully loaded. </p>
