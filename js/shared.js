d02b89cf6765409cc8b4f65d28d1af8f1aea8d23
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-03-01 16:01:38 -0600

    undo user hack

--- js/shared.js
+++ js/shared.js
@@ -4,16 +4,16 @@
 let witnessesObj = {}
 
 //For when we test, so we can easily find and blow away junk data
-setTimeout(() => {
-    document.querySelectorAll("input[deer-key='creator']").forEach(el => {
-        el.value="BryanRefactor"
-        el.setAttribute("value", "BryanRefactor")
-    })
-    document.querySelectorAll("form").forEach(el => {
-        el.setAttribute("deer-creator", "BryanRefactor")
-    })
-    window.GOG_USER["http://store.rerum.io/agent"] = "BryanDelete"
-}, 4000)
+// setTimeout(() => {
+//     document.querySelectorAll("input[deer-key='creator']").forEach(el => {
+//         el.value="BryanDelete"
+//         el.setAttribute("value", "BryanDelete")
+//     })
+//     document.querySelectorAll("form").forEach(el => {
+//         el.setAttribute("deer-creator", "BryanDelete")
+//     })
+//     window.GOG_USER["http://store.rerum.io/agent"] = "BryanDelete"
+// }, 4000)
 
 let __constants = {}
 setConstants()
