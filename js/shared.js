8c251a0bd3568fcfdadd9feb49cfaaf9ad466a05
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-08-09 09:55:07 -0500

    Bring in base64 encode/decode logic for shared.js

--- js/shared.js
+++ js/shared.js
@@ -816,3 +816,33 @@ async function showCustomConfirm(message) {
         }, {once: true})
     })
 }
+
+//https://iiif.io/api/content-state/1.0/#61-choice-of-encoding-mechanism
+function encodeContentState(plainContentState) {
+    let uriEncoded = encodeURIComponent(plainContentState)
+    let base64 = btoa(uriEncoded)
+    let base64url = base64.replace(/\+/g, "-").replace(/\//g, "_")
+    let base64urlNoPadding = base64url.replace(/=/g, "")
+    return base64urlNoPadding
+}
+
+function decodeContentState(encodedContentState) {
+    let base64url = restorePadding(encodedContentState);
+    let base64 = base64url.replace(/-/g, '+').replace(/_/g, '/')
+    let base64Decoded = atob(base64)
+    let uriDecoded = decodeURIComponent(base64Decoded)
+    return uriDecoded
+}
+
+function restorePadding(s) {
+    // The length of the restored string must be a multiple of 4
+    let pad = s.length % 4
+    let padding = ""
+    if (pad) {
+        if (pad === 1) {
+            throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+        }
+        s += '===='.slice(0, 4 - pad)
+    }
+    return s + padding
+}
